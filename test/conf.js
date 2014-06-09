156d3fe80e5384b7535ae06c07c03745cc6f97c5
JulienBrks <julienbrks@gmail.com>
2014-06-09 11:38:13 +0800

    修改了gitignore2文件

--- test/conf.js
+++
@@ -1,7 +0,0 @@
-//protractor测试配置文件
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  seleniumServerJar: '/usr/local/lib/node_modules/protractor/selenium/selenium-server-standalone-2.40.0.jar',
-  specs: ['*Spec.js']
-};
-
